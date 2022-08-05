3fba987fdce65d94cb266c9e1b796d59e44ce888
juramarin <marin.juranko@gmail.com>
2022-08-05 12:21:11 +0200

    1 file modified - RxJS Operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { of, from } from 'rxjs';
+import { of, from, map, tap, take } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -10,14 +10,26 @@ export class AppComponent implements OnInit {
   name = 'Promijeni Angular ' + VERSION.major;
 
   ngOnInit(): void {
-    of(2, 4, 6, 8, 10, 12, 14).subscribe(item => console.log(item));
+    of(2, 4, 6, 8, 10, 12, 14).subscribe((item) => console.log(item));
 
     from([21, 42, 634, 8, 110, 12, 514]).subscribe({
       next: (item) => console.log(`Emitted item is ${item}`),
       error: (err) => console.log(`Emitted item is ${err}`),
-      complete: () => console.log('completed!')
+      complete: () => console.log('completed!'),
     });
 
-    // throw new Error('Method not implemented.');
+    of('Apple1', 'Apple2', 'Apple3').subscribe({
+      next: (item) => console.log(`Emitted item is ${item}`),
+      error: (err) => console.log(`Emitted item is ${err}`),
+      complete: () => console.log('completed!'),
+    });
+
+    of(2, 4, 6, 8, 10, 12, 14)
+      .pipe(
+        tap((item) => console.log(`1 log ${item}`)),
+        map((item) => item * 5),
+        tap((item) => console.log(`2 log ${item}`)),
+        take(3)
+      ).subscribe((item) => console.log(`3 log ${item}`));
   }
 }
